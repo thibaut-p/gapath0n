e0ab9691dad7fa6bc4a5e5125e73ce14298e6142
Thibaut Possompès <thibaut.possompes@edfenergy.com>
2015-07-19 09:43:23 +0100

    Python script update

--- python/emebed_extract.py
+++ python/emebed_extract.py
@@ -1,19 +1,11 @@
 
 import urllib3
-import http.cookiejar
-import sys
 import csv
-import time
-import datetime
 import json
-import pandas as pd
 from pprint import pprint
-import string
 
 
 class AvailableProgrammes:
-
-
     @property
     def programmes(self):
         txdata = None
@@ -27,15 +19,10 @@ class AvailableProgrammes:
         self._programmes = json.loads(req.data.decode("utf-8"))
         return self._programmes
 
-
-
-
 class Programme:
-
     def __init__(self, metadata):
         self.metadata = metadata
         self.url = ''
-
 
     @property
     def projects(self):
@@ -50,7 +37,6 @@ class Programme:
         self._projects = json.loads(req.data.decode("utf-8"))
 
         return self._projects
-
 
 class Project:
     def __init__(self, metadata):
@@ -81,24 +67,50 @@ class Entity:
                         ('primary_energy_requirement' in profile['profile_data'] and profile['profile_data']['primary_energy_requirement'] is not None):
 
                         keep |= True
-
         return keep
 
     def fully_valid(self):
-        keep = False
+        keep = True
+        measures_found = True
+
         if len(self.metadata['profiles']) > 0:
                 for profile in self.metadata['profiles']:
                     if ('space_heating_requirement' in profile['profile_data'] and profile['profile_data']['space_heating_requirement'] is not None) and \
                         ('ber' in profile['profile_data'] and profile['profile_data']['ber'] is not None) and \
                         ('primary_energy_requirement' in profile['profile_data'] and profile['profile_data']['primary_energy_requirement'] is not None):
 
-                        keep |= True
+                        keep &= True
 
-        return keep
+                        if len(self.metadata['devices']) > 0:
+                            for device in self.metadata['devices']:
+                                if len(device['readings']) > 0:
+                                    for reading in device['readings']:
+                                        if reading['upper_ts'] is not None:
+                                            measures_found &= True
+                                else:
+                                    measures_found &= False
+                        else:
+                            measures_found &= False
+
+                    else:
+                        keep &= False
+                        measures_found &= False
+        else:
+            keep = False
+            measures_found = False
+
+        if keep:
+            keep = 1
+        else:
+            keep = 0
+        if measures_found:
+            measures_found = 1
+        else:
+            measures_found = 0
+        return keep, measures_found
 
     def profiles_exists(self):
         return 'profiles' in self.metadata
-
 
     @property
     def profiles(self):
@@ -110,11 +122,9 @@ class Entity:
 
 
 def main():
-
     programmes = AvailableProgrammes()
     programmes_data = programmes.programmes
 
-    # pprint(programmes_data)
     nb_programs = 0
     nb_projects = 0
     nb_entities = 0
@@ -122,15 +132,15 @@ def main():
 
     file = open('all_buildings.csv', 'w',  newline='')
     csvFile = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    csvFile.writerow(['programme_name', 'programme_id', 'project_name', 'project_id', 'project_url', 'property_code', 'entity_id', 'correct_profile', 'fully_correct_profile', 'space_heating_requirement','ber', 'primary_energy_requirement', 'profile_event_type'])
+    csvFile.writerow(['programme_name', 'programme_id', 'project_name', 'project_id', 'project_url', 'property_code', 'entity_id', 'correct_profile', 'fully_correct_profile', 'fcorrect_measures', 'space_heating_requirement','ber', 'primary_energy_requirement', 'profile_event_type'])
 
     for program_data in programmes_data:
         nb_programs += 1
         programme = Programme(program_data)
         # pprint(programme.projects)
 
-        # if programme.metadata['name'] != 'Building Performance Evaluation':
-        #     continue
+        if programme.metadata['name'] != 'Building Performance Evaluation':
+            continue
 
         for project_data in programme.projects:
             nb_projects += 1
@@ -148,7 +158,7 @@ def main():
                 else:
                     correct_profile = False
 
-                fully_correct_profile = entity.fully_valid()
+                fully_correct_profile, fcorrect_measures = entity.fully_valid()
 
                 shr = None
                 ber = None
@@ -182,78 +192,17 @@ def main():
                                           entity.metadata['entity_id'],
                                           correct_profile,
                                           fully_correct_profile,
+                                          fcorrect_measures,
                                           shr,
                                           ber,
                                           per,
                                           profile_event_type])
-
-
 
     print('nb_programs ' + str(nb_programs))
     print('nb_projects ' + str(nb_projects))
     print('nb_entities ' + str(nb_entities))
     print('nb_valid_entities ' + str(nb_valid_entities))
     file.close()
-
-
-
-
-
-    # for program in programmes_data:
-    #     print(program['name'])
-    #     print('http://www.getembed.com'+program['projects'])
-    #
-    #
-    #
-    #
-    # txdata = None
-    # http = urllib3.PoolManager()
-    # txheaders =  {'User-agent' : 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
-    # url = 'http://www.getembed.com/4/programmes/bfb6e716f87d4f1a333fd37d5c3679b2b4b6d87f/projects/'
-    #
-    # req = http.request('GET', url, txdata, txheaders)
-    # print(req.status)
-    # projects_data = json.loads(req.data.decode("utf-8"))
-    #
-    #
-    # file = open('buildings.csv', 'w',  newline='')
-    # csvFile = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    # csvFile.writerow(['project_name', 'project_url', 'property_code', 'entity_id', 'space_heating_requirement', 'primary_energy_requirement', 'ber', 'sap'])
-    # # file.close()
-    #
-    #
-    # count = 0
-    #
-    # for project in projects_data:
-    #     url = 'http://www.getembed.com/4/projects/'+project['project_id']+'/entities/'
-    #     req = http.request('GET', url, txdata, txheaders)
-    #     entities_data = json.loads(req.data.decode("utf-8"))
-    #     for entity in entities_data['entities']:
-    #         if len(entity['profiles'])>0:
-    #
-    #             for profile in entity['profiles']:
-    #                 if 'space_heating_requirement' in profile['profile_data'] and                     'ber' in profile['profile_data'] and                     'sap_rating' in profile['profile_data'] and                     'primary_energy_requirement' in profile['profile_data'] and                     profile['profile_data']['space_heating_requirement'] is not None and                     profile['profile_data']['primary_energy_requirement'] is not None and                     profile['profile_data']['ber'] is not None and                     profile['profile_data']['sap_rating'] is not None:
-    #                     count +=1
-    #
-    #                     print('*****************************')
-    #                     print(project['name'])
-    #                     print(url)
-    #                     print('space_heating_requirement '+ str(profile['profile_data']['space_heating_requirement']))
-    #                     print('primary_energy_requirement ' + str(profile['profile_data']['primary_energy_requirement']))
-    #                     print('ber ' + str(profile['profile_data']['ber']))
-    #                     print('sap rating ' + str(profile['profile_data']['sap_rating']))
-    #                     csvFile.writerow([project['name'],
-    #                                       url,
-    #                                       entity['property_code'],
-    #                                       entity['entity_id'],
-    #                                       profile['profile_data']['space_heating_requirement'],
-    #                                       profile['profile_data']['primary_energy_requirement'],
-    #                                       profile['profile_data']['ber'],
-    #                                       profile['profile_data']['sap_rating']])
-    #
-    # print(count)
-    # file.close()
-
 
 if __name__ == "__main__":
     main()
